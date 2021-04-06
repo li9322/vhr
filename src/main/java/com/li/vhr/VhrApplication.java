388f2bdc915bd5c96a07bc6bdaa61058ec75e93f
li <1172892394@qqcom>
2021-04-06 22:57:13 +0800

    添加bean和mapper文件

--- src/main/java/com/li/vhr/VhrApplication.java
+++ src/main/java/com/li/vhr/VhrApplication.java
@@ -1,9 +1,11 @@
 package com.li.vhr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.li.vhr.mapper")
 public class VhrApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/li/vhr/mapper/HrMapper.java
@@ -0,0 +1,17 @@
+package com.li.vhr.mapper;
+
+import com.li.vhr.model.Hr;
+
+public interface HrMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Hr record);
+
+    int insertSelective(Hr record);
+
+    Hr selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Hr record);
+
+    int updateByPrimaryKey(Hr record);
+}
